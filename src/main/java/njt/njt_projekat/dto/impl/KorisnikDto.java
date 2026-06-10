1c3909561dca94d5aa43569ef875d3f16186d478
Sara Radulovic <sr20220072@student.fon.bg.ac.rs>
2026-06-10 15:19:45 +0200

    Dodata validacija u KorisnikDto

--- src/main/java/njt/njt_projekat/dto/impl/KorisnikDto.java
+++ src/main/java/njt/njt_projekat/dto/impl/KorisnikDto.java
@@ -5,6 +5,8 @@
 package njt.njt_projekat.dto.impl;
 
 import java.util.List;
+
+import jakarta.validation.constraints.*;
 import njt.njt_projekat.dto.Dto;
 import njt.njt_projekat.entity.impl.Uloga;
 
@@ -13,98 +15,107 @@ import njt.njt_projekat.entity.impl.Uloga;
  * @author Sara
  */
 public class KorisnikDto implements Dto {
-    private Long korisnikId;
-    private String ime;
-    private String prezime;
-    private String korisnickoIme;
-    private String email; 
-    private Uloga uloga = Uloga.USER;
-    private String adresa;
+	private Long korisnikId;
 
-    private List<PorudzbinaDto> porudzbine;
+	@NotBlank(message = "Niste uneli ime!")
+	@Size(min = 3, max = 50, message = "Ime mora imati izmedju 3 i 50 karaktera")
+	private String ime;
 
-    public KorisnikDto() {
-    }
+	@Size(min = 3, max = 100, message = "Prezime mora imati izmedju 3 i 100 karaktera")
+	private String prezime;
 
-    public KorisnikDto(Long korisnikId, String ime, String prezime, String korisnickoIme, String email, String adresa,Uloga uloga, List<PorudzbinaDto> porudzbine) {
-        this.korisnikId = korisnikId;
-        this.ime = ime;
-        this.prezime = prezime;
-        this.korisnickoIme = korisnickoIme;
-        this.email = email;
-        this.adresa = adresa;
-        this.uloga=uloga;
-        this.porudzbine = porudzbine;
-    }
+	@NotBlank(message = "Niste uneli korisnicko ime!")
+	@Size(min = 3, max = 50, message = "Korisnicko ime mora imati izmedju 3 i 50 karaktera")
+	private String korisnickoIme;
 
-    public Long getKorisnikId() {
-        return korisnikId;
-    }
+	@NotBlank(message = "Niste uneli email!")
+	@Email(message = "Email nije u ispravnom formatu")
+	private String email;
 
-    public void setKorisnikId(Long korisnikId) {
-        this.korisnikId = korisnikId;
-    }
+	private Uloga uloga = Uloga.USER;
 
-    public String getIme() {
-        return ime;
-    }
+	@Size(min = 5, max = 255, message = "Adresa mora imati izmedju 5 i 255 karaktera")
+	private String adresa;
 
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
+	private List<PorudzbinaDto> porudzbine;
 
-    public String getPrezime() {
-        return prezime;
-    }
+	public KorisnikDto() {
+	}
 
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
-    }
+	public KorisnikDto(Long korisnikId, String ime, String prezime, String korisnickoIme, String email, String adresa,
+			Uloga uloga, List<PorudzbinaDto> porudzbine) {
+		this.korisnikId = korisnikId;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.korisnickoIme = korisnickoIme;
+		this.email = email;
+		this.adresa = adresa;
+		this.uloga = uloga;
+		this.porudzbine = porudzbine;
+	}
 
-    public String getKorisnickoIme() {
-        return korisnickoIme;
-    }
+	public Long getKorisnikId() {
+		return korisnikId;
+	}
 
-    public void setKorisnickoIme(String korisnickoIme) {
-        this.korisnickoIme = korisnickoIme;
-    }
+	public void setKorisnikId(Long korisnikId) {
+		this.korisnikId = korisnikId;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getIme() {
+		return ime;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
 
-  
-    public Uloga getUloga() {
-        return uloga;
-    }
+	public String getPrezime() {
+		return prezime;
+	}
 
-    public void setUloga(Uloga uloga) {
-        this.uloga = uloga;
-    }
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
 
-    public String getAdresa() {
-        return adresa;
-    }
+	public String getKorisnickoIme() {
+		return korisnickoIme;
+	}
 
-    public void setAdresa(String adresa) {
-        this.adresa = adresa;
-    }
+	public void setKorisnickoIme(String korisnickoIme) {
+		this.korisnickoIme = korisnickoIme;
+	}
 
-    public List<PorudzbinaDto> getPorudzbine() {
-        return porudzbine;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setPorudzbine(List<PorudzbinaDto> porudzbine) {
-        this.porudzbine = porudzbine;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    
+	public Uloga getUloga() {
+		return uloga;
+	}
 
-   
-    
-    
+	public void setUloga(Uloga uloga) {
+		this.uloga = uloga;
+	}
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	public List<PorudzbinaDto> getPorudzbine() {
+		return porudzbine;
+	}
+
+	public void setPorudzbine(List<PorudzbinaDto> porudzbine) {
+		this.porudzbine = porudzbine;
+	}
+
 }
