c0a3c64c7da925de05e85f59a0d498f9b3208253
Sara Radulovic <sr20220072@student.fon.bg.ac.rs>
2026-06-10 12:54:10 +0200

    Dodata veza ka dostavljacu u klasi Porudzbina

--- src/main/java/njt/njt_projekat/entity/impl/Porudzbina.java
+++ src/main/java/njt/njt_projekat/entity/impl/Porudzbina.java
@@ -26,100 +26,114 @@ import java.util.List;
  * @author Sara
  */
 @Entity
-@Table(name="porudzbine")
+@Table(name = "porudzbine")
 public class Porudzbina {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long porudzbinaId;
-    
-    @Column(nullable=false)
-    private Double ukupanIznos;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long porudzbinaId;
 
-    @Column(nullable=false)
-    private Date datum = new Date();
-    
-    @Enumerated(EnumType.STRING)
-    @Column(nullable=false, length=20)
-    private StatusPorudzbine status = StatusPorudzbine.KREIRANA;
+	@Column(nullable = false)
+	private Double ukupanIznos;
 
-    @ManyToOne(optional=false,fetch=FetchType.LAZY)
-    @JoinColumn(name = "korisnikId", nullable=false)
-    private Korisnik korisnik;
-    
-    @OneToMany(mappedBy = "porudzbina", cascade=CascadeType.ALL, orphanRemoval = true) //uklanja sirocice
-    private List<StavkaPorudzbine> stavkePorudzbine= new ArrayList<>();
+	@Column(nullable = false)
+	private Date datum = new Date();
 
-    public Porudzbina() {
-    }
-    public Porudzbina(Long porudzbinaId) {
-        this.porudzbinaId = porudzbinaId;
-    }
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private StatusPorudzbine status = StatusPorudzbine.KREIRANA;
 
-    public Porudzbina(Long porudzbinaId, Double ukupanIznos, Korisnik korisnik,Date datum,StatusPorudzbine status, List<StavkaPorudzbine> stavkePorudzbine) {
-        this.porudzbinaId = porudzbinaId;
-        this.ukupanIznos = ukupanIznos;
-        this.korisnik = korisnik;
-        this.datum= datum;
-        this.status=status;
-        this.stavkePorudzbine = stavkePorudzbine;
-    }
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "korisnikId", nullable = false)
+	private Korisnik korisnik;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "dostavljacId")
+	private Dostavljac dostavljac;
 
-    public Long getPorudzbinaId() {
-        return porudzbinaId;
-    }
+	@OneToMany(mappedBy = "porudzbina", cascade = CascadeType.ALL, orphanRemoval = true) // uklanja sirocice
+	private List<StavkaPorudzbine> stavkePorudzbine = new ArrayList<>();
 
-    public void setPorudzbinaId(Long porudzbinaId) {
-        this.porudzbinaId = porudzbinaId;
-    }
+	public Porudzbina() {
+	}
 
-    public Double getUkupanIznos() {
-        return ukupanIznos;
-    }
+	public Porudzbina(Long porudzbinaId) {
+		this.porudzbinaId = porudzbinaId;
+	}
 
-    public void setUkupanIznos(Double ukupanIznos) {
-        this.ukupanIznos = ukupanIznos;
-    }
+	public Porudzbina(Long porudzbinaId, Double ukupanIznos, Korisnik korisnik, Date datum, StatusPorudzbine status,
+			List<StavkaPorudzbine> stavkePorudzbine) {
+		this.porudzbinaId = porudzbinaId;
+		this.ukupanIznos = ukupanIznos;
+		this.korisnik = korisnik;
+		this.datum = datum;
+		this.status = status;
+		this.stavkePorudzbine = stavkePorudzbine;
+	}
 
-    public Date getDatum() {
-        return datum;
-    }
+	public Long getPorudzbinaId() {
+		return porudzbinaId;
+	}
 
-    public void setDatum(Date datum) {
-        this.datum = datum;
-    }
+	public void setPorudzbinaId(Long porudzbinaId) {
+		this.porudzbinaId = porudzbinaId;
+	}
 
-    public Korisnik getKorisnik() {
-        return korisnik;
-    }
+	public Double getUkupanIznos() {
+		return ukupanIznos;
+	}
 
-    public void setKorisnik(Korisnik korisnik) {
-        this.korisnik = korisnik;
-    }
+	public void setUkupanIznos(Double ukupanIznos) {
+		this.ukupanIznos = ukupanIznos;
+	}
 
-    public List<StavkaPorudzbine> getStavkePorudzbine() {
-        return stavkePorudzbine;
-    }
+	public Date getDatum() {
+		return datum;
+	}
 
-    public void setStavkePorudzbine(List<StavkaPorudzbine> stavkePorudzbine) {
-        this.stavkePorudzbine = stavkePorudzbine;
-    }
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
 
-    public StatusPorudzbine getStatus() {
-        return status;
-    }
+	public Korisnik getKorisnik() {
+		return korisnik;
+	}
 
-    public void setStatus(StatusPorudzbine status) {
-        this.status = status;
-    }
-    
-    
-    public void addItem(StavkaPorudzbine stavka){
-        stavka.setPorudzbina(this);
-        this.stavkePorudzbine.add(stavka);
-    }
-    public void removeItem(StavkaPorudzbine stavka){
-        stavka.setPorudzbina(null);
-        this.stavkePorudzbine.remove(stavka);
-    }
+	public void setKorisnik(Korisnik korisnik) {
+		this.korisnik = korisnik;
+	}
+
+	public List<StavkaPorudzbine> getStavkePorudzbine() {
+		return stavkePorudzbine;
+	}
+
+	public void setStavkePorudzbine(List<StavkaPorudzbine> stavkePorudzbine) {
+		this.stavkePorudzbine = stavkePorudzbine;
+	}
+
+	public StatusPorudzbine getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPorudzbine status) {
+		this.status = status;
+	}
+
+	public void addItem(StavkaPorudzbine stavka) {
+		stavka.setPorudzbina(this);
+		this.stavkePorudzbine.add(stavka);
+	}
+
+	public void removeItem(StavkaPorudzbine stavka) {
+		stavka.setPorudzbina(null);
+		this.stavkePorudzbine.remove(stavka);
+	}
+
+	public Dostavljac getDostavljac() {
+		return dostavljac;
+	}
+
+	public void setDostavljac(Dostavljac dostavljac) {
+		this.dostavljac = dostavljac;
+	}
+
 }
