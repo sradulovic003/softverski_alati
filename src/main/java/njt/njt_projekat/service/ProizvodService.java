02a0495782ef2030e666fa5e33627fdbbe573ae6
Sara Radulovic <sr20220072@student.fon.bg.ac.rs>
2026-06-11 16:52:49 +0200

    Dokumentovan servis ProizvodService

--- src/main/java/njt/njt_projekat/service/ProizvodService.java
+++ src/main/java/njt/njt_projekat/service/ProizvodService.java
@@ -14,40 +14,89 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
+ * Servis koji implementira poslovnu logiku za upravljanje proizvodima.
  *
- * @author Sara
+ * Omogucava kreiranje, pregled, azuriranje i brisanje proizvoda,
+ * kao i pregled proizvoda po poslasticari.
+ * Koristi ProizvodRepository za pristup bazi podataka i
+ * ProizvodMapper za konverziju izmedju entiteta i DTO objekata.
+ *
+ * @author Sara Radulovic
+ * @version 1.0
  */
 @Service
 public class ProizvodService {
+	
+	/**
+     * Repozitorijum za pristup podacima o proizvodima u bazi podataka.
+     */
     private final ProizvodRepository proizvodRepository;
+    
+    /**
+     * Maper za konverziju izmedju entiteta Proizvod i DTO objekta ProizvodDto.
+     */
     private final ProizvodMapper proizvodMapper;
 
+    /**
+     * Kreira objekat klase ProizvodService sa unetim zavisnostima.
+     *
+     * @param proizvodRepository Repozitorijum za pristup podacima o proizvodima.
+     * @param proizvodMapper Maper za konverziju izmedju entiteta i DTO objekata.
+     */
     @Autowired
     public ProizvodService(ProizvodRepository proizvodRepository, ProizvodMapper proizvodMapper) {
         this.proizvodRepository = proizvodRepository;
         this.proizvodMapper = proizvodMapper;
     }
     
+    /**
+     * Vraca listu svih proizvoda iz baze podataka.
+     *
+     * @return lista svih proizvoda kao lista objekata klase ProizvodDto.
+     */
     public List<ProizvodDto> findAll(){
         return proizvodRepository.findAll().stream().map(proizvodMapper::toDto).collect(Collectors.toList());
     }
 
+    /**
+     * Kreira novi proizvod u bazi podataka.
+     *
+     * @param proizvodDto Podaci o novom proizvodu.
+     * @return kreirani proizvod kao objekat klase ProizvodDto.
+     */
     public ProizvodDto create(ProizvodDto proizvodDto) {
         Proizvod proizvod = proizvodMapper.toEntity(proizvodDto);
         proizvodRepository.save(proizvod);
         return proizvodMapper.toDto(proizvod);
     }
 
+    /**
+     * Brise proizvod sa unetim identifikatorom iz baze podataka.
+     *
+     * @param id Identifikator proizvoda koji se brise.
+     */
     public void deleteById(Long id) {
         proizvodRepository.deleteById(id);
     }
 
+    /**
+     * Azurira postojeci proizvod u bazi podataka.
+     *
+     * @param proizvodDto Novi podaci o proizvodu.
+     * @return azurirani proizvod kao objekat klase ProizvodDto.
+     */
     public ProizvodDto update(ProizvodDto proizvodDto) {
         Proizvod proizvod = proizvodMapper.toEntity(proizvodDto);
         proizvodRepository.save(proizvod);
         return proizvodMapper.toDto(proizvod);
     }
 
+    /**
+     * Vraca listu svih proizvoda koji pripadaju poslasticari sa unetim identifikatorom.
+     *
+     * @param id Identifikator poslasticare.
+     * @return lista proizvoda poslasticare kao lista objekata klase ProizvodDto.
+     */
     public List<ProizvodDto> findByPoslasticaraId(Long id) {
         return proizvodRepository.findByPoslasticaraId(id).stream().map(proizvodMapper::toDto).collect(Collectors.toList());
     }
